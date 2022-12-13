f2488c59f95f44b63fefa0038e1d802b82973b00
Trần Ngọc Quý <quytn@etc.vn>
2022-12-13 09:39:10 +0700

    sort update

--- src/SortSearch/Sort.java
+++ src/SortSearch/Sort.java
@@ -40,13 +40,13 @@ public class Sort {
 
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
-        int i, j, temp;     
-        for (i = 0; i < n; i++){      
-            for (j = i + 1; j < n; j++){     
-                if (arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        int i, j, temp;
+        for (i = 0; i < n - 1; i++){
+            for (j = 0; j < n - i - 1; j++){
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
